714b0b878c7703859446108ba0a4456fe27ae5ab
Eivind Stray <eivindhstray@gmail.com>
2020-10-20 09:28:14 +0200

    cross_product_matrix verified working

--- graded_2/utils.py
+++ graded_2/utils.py
@@ -3,11 +3,24 @@ from mytypes import ArrayLike
 
 
 def cross_product_matrix(n: ArrayLike, debug: bool = True) -> np.ndarray:
+
+    ''' 
+    VERIFIED WORKING
+    '''
+    
     assert len(n) == 3, f"utils.cross_product_matrix: Vector not of length 3: {n}"
     vector = np.array(n, dtype=float).reshape(3)
 
     S = np.zeros((3, 3))  # TODO: Create the cross product matrix
-    raise NotImplementedError
+    n_1 = n[0]
+    n_2 = n[1]
+    n_3 = n[2]
+    S[0,1] = - n_3
+    S[1,0] = n_3
+    S[0,2] = n_2
+    S[2,0] = - n_2
+    S[1,2] = - n_1
+    S[2,1] = n_1
     if debug:
         assert S.shape == (
             3,
@@ -18,3 +31,4 @@ def cross_product_matrix(n: ArrayLike, debug: bool = True) -> np.ndarray:
         ), f"utils.cross_product_matrix: Result is not skew-symmetric: {S}"
 
     return S
+
